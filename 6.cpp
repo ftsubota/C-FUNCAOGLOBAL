9a4a1d94ab46a22ae2a68b43dd512d3a3a7341cc
Felipe Tsubota <88908355+ftsubota@users.noreply.github.com>
2021-10-20 19:57:10 -0300

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,19 +1,58 @@
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#define LIMITE_DADO 7
+
 using namespace std;
 
-int menor=1, maior=6;	
-
-int dado(int menor, int maior){
-	return rand()%(maior-menor+1) + menor;
+int dado(){
+	srand(time(NULL));
+	int sorteio = rand()% LIMITE_DADO;
+	while (sorteio == 0){
+		sorteio = rand()% LIMITE_DADO;
+	}
+		return sorteio;
 }
 
 int main(){
-	setlocale(LC_ALL,"Portuguese"); 
+	int lado1 = 0;
+	int lado2 = 0;
+	int lado3 = 0;
+	int lado4 = 0;
+	int lado5 = 0;
+	int lado6 = 0;
 	
-	srand((unsigned)time(0));
-	int sorteio = dado(1,6);
-	cout << "O numero aleatório é: " << sorteio;
-	return 0;
+	for (int i = 0; i < 100000; i++){
+		int sorteio = dado();
+		switch (sorteio){
+			case 1:
+				lado1++;
+			break;
+			case 2:
+				lado2++;
+			break;
+			case 3:
+				lado3++;
+			break;
+			case 4:
+				lado4++;
+			break;
+			case 5:
+				lado5++;
+			break;
+			case 6:
+				lado6++;
+			break;
+		}
+		cout << sorteio;
+		
+	}
+	cout << "\n -------------- \n";
+	cout << "\n Lado 1: " << lado1;
+	cout << "\n Lado 2: " << lado2;
+	cout << "\n Lado 3: " << lado3;
+	cout << "\n Lado 4: " << lado4;
+	cout << "\n Lado 5: " << lado5;
+	cout << "\n Lado 6: " << lado6;
+		return 0;
 }
